89611410ac79393555fdfd0afeeecbbce6c0a971
garyeung <5206130+garyeung@users.noreply.github.com>
2026-01-08 11:08:00 +0800

    refactoring

--- src/components/compounds/Header.tsx
+++ src/components/compounds/Header.tsx
@@ -4,6 +4,8 @@ import Nav from "@/components/bases/Nav";
 import useScreeSize from "@/hooks/useScreenSize";
 import Caption from "../bases/Caption";
 import ReactiveImage from "../bases/ReactiveImage";
+import "./Header.less";
+import { useRef } from "react";
 
 interface Props {
     bgDesktop: string,
@@ -14,19 +16,24 @@ interface Props {
 const Header = (props: Props) => {
     const screenSize = useScreeSize();
 
+    const headerRef = useRef<HTMLDivElement>(null);
+    
+
     return (
-        <header className="header">
-            <Logo/>
-            {
-                screenSize.width > 768 ? <Menu/> : <Nav/>
-            }
-            <Caption text={props.caption}/>
+        <header className="header" ref={headerRef}>
             <div className="header-bg">
                 <ReactiveImage 
                     mobileSrc={props.bgMobile}
                     desktopSrc={props.bgDesktop}
                     alt={props.bgName}
                 />
+            </div>
+            <div className="header-body">
+              <Logo/>
+              {
+                  screenSize.width > 768 ? <Nav/> : <Menu/>
+              }
+              <Caption text={props.caption}/>
             </div>
         </header>
     )
