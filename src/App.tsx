efe8d3969828ff8b8f00e5df4f8f43b155e35dad
garyeung <5206130+garyeung@users.noreply.github.com>
2026-01-11 15:16:37 +0800

    implment fetch data hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,57 @@
-import { useEffect, useState } from 'react';
 import './App.less';
-import * as datas from './datas';
 import Hero from '@/components/compounds/Hero';
 import Interactive from '@/components/compounds/Interactive';
-import Creations, { CreationsProps } from '@/components/compounds/Creations';
-import { fetchCreationsInfo, fetchSocialMediasInfo} from '@/services/store';
-import { ICreationInfo } from '@/interfaces/creationInfo';
+import Creations from '@/components/compounds/Creations';
+import { fetchCreationsInfo, fetchHeroInfo, fetchInteractiveInfo, fetchSocialMediasInfo } from '@/services/store';
 import Footer from '@/components/compounds/Footer';
-import ISocialMediaInfo from '@/interfaces/socialmediaInfo';
+import useFetchData from './hooks/useFetchData';
+import { useMemo } from 'react';
 
 function App() {
-  const [creationsInfo, setCreationsInfo] = useState<ICreationInfo[]>([]); // State for creations data
+  const { data: creationsInfo, loading: creationsLoading } = useFetchData(fetchCreationsInfo);
+  const { data: socialmediasInfo, loading: socialmediasLoading } = useFetchData(fetchSocialMediasInfo);
+  const { data: heroInfo, loading: heroLoading } = useFetchData(fetchHeroInfo);
+  const { data: interactiveInfo, loading: interactiveLoading } = useFetchData(fetchInteractiveInfo);
 
   const companyName = "Loopstudios";
-  const [socialmediasInfo, setSocialmediasInfo] = useState<ISocialMediaInfo[]>([]); // State for socialmedias data
 
-
-  // Fetch creations data when component mounts
-  useEffect(() => {
-    const loadCreations = async () => {
-      const data = await fetchCreationsInfo();
-      setCreationsInfo(data);
-    };
-    loadCreations();
-  }, []); // Empty dependency array ensures this runs once on mount
-
-  useEffect(() => {
-    const loadSocialMedias = async () => {
-      const data = await fetchSocialMediasInfo();
-      setSocialmediasInfo(data);
-    };
-    loadSocialMedias();
-
-  }, [])
-
-  const creationsProps: CreationsProps = {
-    creationsInfo: creationsInfo.map((creation) => ({
+  const creationsProps = useMemo(() => ({
+    creationsInfo: creationsInfo?.map((creation) => ({
       mobileImg: creation.pictures.mobile,
       desktopImg: creation.pictures.desktop,
       caption: creation.title,
       path: "#", // simply demo
-    })),    
-  } 
+    })) || [],
+  }), [creationsInfo]);
 
+  const isLoading = creationsLoading || socialmediasLoading || heroLoading || interactiveLoading;
+
+  if (isLoading) {
+    return <div className='loading'>Loading...</div>;
+  }
+
+  // Render the full component once all data is loaded
   return (
-    <>
-    <Hero 
-      bgDesktop={datas.bgDesUrl}
-      bgMobile={datas.bgMobUrl}
-      bgName="hero"
-      caption={datas.headerTitle}
-    />
-    <Interactive 
-      mobileImg={datas.introImgMobile}
-      desktopImg={datas.introImgDesk}
-      title={datas.introTitle}
-      introduction={datas.introText}
-    />
-    <Creations creationsInfo={creationsProps.creationsInfo} /> 
-    <Footer companyName={companyName} socialMedias={socialmediasInfo}/>
-    </>
+    <div className='container'>
+      {heroInfo && (
+        <Hero
+          bgDesktop={heroInfo.bgDesktop}
+          bgMobile={heroInfo.bgMobile}
+          bgName="hero"
+          caption={heroInfo.caption}
+        />
+      )}
+      {interactiveInfo && (
+        <Interactive
+          mobileImg={interactiveInfo.pictureMobile}
+          desktopImg={interactiveInfo.pictureDesktop}
+          title={interactiveInfo.title}
+          introduction={interactiveInfo.text}
+        />
+      )}
+      <Creations creationsInfo={creationsProps.creationsInfo} />
+      {socialmediasInfo && <Footer companyName={companyName} socialMedias={socialmediasInfo} />}
+    </div>
   )
 }
 
